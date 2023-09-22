52fc5eeea69fa2bca1ec77f92d39992e82e9916a
Angjelo Mana <hc7676@wayne.edu>
2023-09-22 11:37:14 -0400

    New Commit

--- components/ProjectItems.jsx
+++ components/ProjectItems.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-const ProjectItems = ({title,backgroundImg,framework}) => {
+const ProjectItems = ({title,backgroundImg,framework,ProjectUrl}) => {
 return (
 <div className='reletive flex items-center justify-between h-auto w-full shadow-xl shadow-gray-800 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff] '>
     <Image className='rounded-xl group-hover:opacity-10' src={backgroundImg} alt='/'/>
     <div className='hidden group-hover:block absolute translate-x-[100%] translate-y-[0%]'>
         <h3 className='text-2xl text-white tracking-wider text-center'>{title}</h3>
         <p className='pb-4 pt-2 text-white text-center'>{framework}</p>
-        <Link href={'https://github.com/Angjelo154'}>
+        <Link href={ProjectUrl}>
             <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
         </Link>
     </div>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -17,6 +17,8 @@ const Projects = () => {
                 title='LEVIN Website'
                 backgroundImg={LEVINimg}
                 framework='React JS'
+                ProjectUrl='https://github.com/WSU-4110/Levin'
+                
                 
             />
             <p>Collaborated and helped design a full-stack website that allows any user to create dynamic sitemaps. This website uses JavaScript, HTML, CSS, SQL, Spring Boot, And React JS.
@@ -25,7 +27,8 @@ const Projects = () => {
             <ProjectItems
                 title='Tesla Website'
                 backgroundImg={TeslaImg} 
-                framework='Redux'  
+                framework='Redux'
+                ProjectUrl='https://github.com/Angjelo154/Tesla-Clone'  
             />
             <p>Built an online website using JavaScript, Styled Components, and Redux that allows users to experience the different models of cars Tesla has to offer. 
                This app also features important information that offers a unique experience to users learning more about electric cars.
@@ -34,6 +37,7 @@ const Projects = () => {
                 title='NFT Project'
                 backgroundImg={NFTimg}
                 framework='Java'
+                ProjectUrl='https://github.com/Angjelo154'
             />
             <p>Created a full-stack NFT website using Java, HTML, CSS, and MySQL to implement interfaces that simulate the buying, trading, and selling of NFT’s.
             Implemented a Java control servlet to control the application’s flow from one user interaction to another.
